43a5b033e5250b31fd19f681f70b65c72e5f2efd
Tony Guimelli <tony.guimelli@gmail.com>
2021-06-04 10:28:47 -0400

    Update Get-Cookie.ps1

--- Get-Cookie.ps1
+++ Get-Cookie.ps1
@@ -1,28 +1,33 @@
 function Get-Cookie {
-    <#  
+    <#
     .SYNOPSIS
         Gets cookies.
     .DESCRIPTION
         See synopsis.
-    .EXAMPLE        
-        Get-Cookie -Uri "https://www.linkedin.com"                       
+    .EXAMPLE
+        Get-Cookie -Uri "https://www.linkedin.com"
     .PARAMETER Uri
         Specifies the Uniform Resource Identifier (URI) of the internet resource to which the web request is sent.
     .LINK
-        https://www.youtube.com/watch?v=BovQyphS8kA        
+        https://www.youtube.com/watch?v=BovQyphS8kA
 #>
     [CmdletBinding()]
     [OutputType([System.Net.Cookie[]])]
     Param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)][System.Uri]$Uri
-    ) 
-    PROCESS {       
+    )
+    PROCESS {
         [System.Net.Cookie[]]$cookies = $null
 
-        Invoke-WebRequest -Uri $Uri -SessionVariable websession | Out-Null
+        try {
+            Invoke-WebRequest -Uri $Uri -SessionVariable websession -ErrorAction Stop | Out-Null
+        }
+        catch {
+            Write-Error -Exception $_.Exception -ErrorAction Stop
+        }
 
         $cookies = $websession.Cookies.GetCookies($Uri)
 
         return $cookies
-    }   
+    }
 }
