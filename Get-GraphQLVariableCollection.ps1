6d9ef4d5544c40a01ac940624bc3ee81df0edb23
Tony Guimelli <tony.guimelli@gmail.com>
2021-11-15 12:24:31 -0500

    Update and rename GraphQLVariableCollection.ps1 to Get-GraphQLVariableCollection.ps1

--- Get-GraphQLVariableCollection.ps1
+++ Get-GraphQLVariableCollection.ps1
@@ -1,24 +1,21 @@
-function Get-GraphQLVariableCollection
-{
+function Get-GraphQLVariableCollection {
     [CmdletBinding()]
     [Alias('ggqlvc')]
     [OutputType([GraphQLVariableCollection])]
     Param
     (
-        [Parameter(Mandatory=$true,
-                   ValueFromPipeline=$true,
-                   Position=0)]$Query
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $true,
+            Position = 0)][ValidateLength(12, 1073741791)][Alias("Mutation", "q", "m")][System.String]$Query
     )
-    BEGIN
-    {
+    BEGIN {
         class GraphQLVariableCollection {
             [string]$Query = ""
             [string]$Parameter = ""
             [string]$Type = ""
         }
     }
-    PROCESS
-    {
+    PROCESS {
         $results = @()
 
         if (($Query.ToLower() -notlike "query*") -and ($Query.ToLower() -notlike "mutation*") ) {
@@ -35,15 +32,15 @@ function Get-GraphQLVariableCollection
         $queryName = ([RegEx]::Match($firstLine, $queryRegex).Groups[0].Value -replace "\(", "").Split(" ")[1]
 
         (([RegEx]::Match($firstLine, $paranRegex).Groups[1]).Value -split ",").Trim() | ForEach-Object {
-             $param = [RegEx]::Replace(($_.Split(":")[0].Trim()), $nonAlphaNumericRegex, "")
-             $paramType = [RegEx]::Replace(($_.Split(":")[1].Trim()), $nonAlphaNumericRegex, "")
+            $param = [RegEx]::Replace(($_.Split(":")[0].Trim()), $nonAlphaNumericRegex, "")
+            $paramType = [RegEx]::Replace(($_.Split(":")[1].Trim()), $nonAlphaNumericRegex, "")
 
-             $gqlvc = [GraphQLVariableCollection]::new()
-             $gqlvc.Query = $queryName
-             $gqlvc.Parameter = $param
-             $gqlvc.Type = $paramType
+            $gqlvc = [GraphQLVariableCollection]::new()
+            $gqlvc.Query = $queryName
+            $gqlvc.Parameter = $param
+            $gqlvc.Type = $paramType
 
-             $results += $gqlvc
+            $results += $gqlvc
         }
 
         return $results
