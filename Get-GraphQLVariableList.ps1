e44101117a27db2f2c67039e21475a984adc21e6
Tony Guimelli <tony.guimelli@gmail.com>
2021-11-15 22:34:14 -0500

    Update Get-GraphQLVariableList.ps1

--- Get-GraphQLVariableList.ps1
+++ Get-GraphQLVariableList.ps1
@@ -13,13 +13,13 @@ function Get-GraphQLVariableList {
     (
         [Parameter(Mandatory = $true,
             ValueFromPipeline = $true,
-            Position = 0)][ValidateLength(12, 1073741791)][Alias("Mutation", "q", "m")][System.String]$Query,
+            Position = 0)][ValidateLength(12, 1073741791)][Alias("Operation", "Mutation")][System.String]$Query,
 
         [Parameter(Mandatory = $false, Position = 1)][Switch]$AsHashtable
     )
     BEGIN {
         class GraphQLVariable {
-            [string]$Query
+            [string]$Operation
             [string]$Parameter
             [string]$Type
         }
@@ -29,21 +29,26 @@ function Get-GraphQLVariableList {
         $ArgumentException = New-Object -TypeName ArgumentException -ArgumentList "Not a valid GraphQL query or mutation. Verify syntax and try again."
 
         # Attempt to determine if value passed to the query parameter is an actual GraphQL query or mutation. If not, throw.
-        if (($Query.ToLower() -notlike "query*") -and ($Query.ToLower() -notlike "mutation*") ) {
+        [string]$trimmedQuery = $Query.Trim()
+
+        if (($trimmedQuery -notlike "query*") -and ($trimmedQuery -notlike "mutation*") ) {
             Write-Error -Exception $ArgumentException -Category InvalidArgument -ErrorAction Stop
         }
 
         # Get the first line of the incoming query or mutation as part of the process to determine the query or mutation name:
-        $firstLine = $Query -split "`r`n" | Select-Object -First 1
+        $firstLine = $trimmedQuery -split "`r`n" | Select-Object -First 1
 
         # Determine query name by determining if query has parameters or not:
         [string]$queryName = ""
+        [bool]$hasParameters = $false
+
         try {
             if (($firstLine -split " ")[1] -notmatch "\(") {
                 $queryName = $firstLine.Split(" ")[1].Trim()
             }
             else {
                 $queryName = $($firstLine.Split("(")[0].Split(" ")[1]).Trim()
+                $hasParameters = $true
             }
         }
         catch {
@@ -55,28 +60,31 @@ function Get-GraphQLVariableList {
             Write-Error -Exception $ArgumentException -Category InvalidArgument -ErrorAction Stop
         }
 
-        # Two regexes; one to determine if parantheses exist, the other for non-alphanumeric.
-        # Used in the following try block to put together results to be returned:
-        $paranRegex = [RegEx]"\((.*)\)"
-        $nonAlphaNumericRegex = [RegEx]"[^a-zA-Z0-9]"
 
+        # List of objects that are returned by default:
         $results = [List[GraphQLVariable]]::new()
-        try {
-            $((([RegEx]::Match($firstLine, $paranRegex).Groups[1]).Value -split ",").Trim()) | ForEach-Object {
-                $param = [RegEx]::Replace(($_.Split(":")[0].Trim()), $nonAlphaNumericRegex, "")
-                $paramType = [RegEx]::Replace(($_.Split(":")[1].Trim()), $nonAlphaNumericRegex, "")
 
-                $gqlvc = [GraphQLVariable]::new()
-                $gqlvc.Query = $queryName
-                $gqlvc.Parameter = $param
-                $gqlvc.Type = $paramType
-                $results.Add($gqlvc)
+        if ($hasParameters) {
+            # Run a regex against the first line looking for property name and type:
+            [string]$queryNameAndTypeRegex = "(?<=\$)[_A-Za-z][_0-9A-Za-z]*:[\s]*[A-Za-z][0-9A-Za-z]*(?=[\!]?[,\)])"
+
+            [regex]::Matches($firstLine, $queryNameAndTypeRegex) |
+            Select-Object -ExpandProperty Value | ForEach-Object {
+                $parameterName = ($_.Split(":")[0]).Trim()
+                $parameterType = ($_.Split(":")[1]).Trim()
+
+                $gqlVariable = [GraphQLVariable]::new()
+                $gqlVariable.Operation = $queryName
+                $gqlVariable.Parameter = $parameterName
+                $gqlVariable.Type = $parameterType
+
+                $results.Add($gqlVariable)
             }
         }
-        catch {
-            $gqlvc = [GraphQLVariable]::new()
-            $gqlvc.Query = $queryName
-            $results += $gqlvc
+        else {
+            $gqlVariable = [GraphQLVariable]::new()
+            $gqlVariable.Operation = $queryName
+            $results.Add($gqlVariable)
         }
 
         # Returns results as a hashtable with the key being the queryname
