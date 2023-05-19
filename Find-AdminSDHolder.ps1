4e82dcaaab2dabe1691b4e89baff8082fc62d524
Tony Guimelli <tony.guimelli@gmail.com>
2023-05-19 19:37:37 -0400

    Update Find-AdminSDHolder.ps1

--- Find-AdminSDHolder.ps1
+++ Find-AdminSDHolder.ps1
@@ -1,6 +1,8 @@
 #-------------------------------------------------- BEGIN Find-AdminSDHolder.ps1 --------------------------------------------------------------------
 Import-Module -Name ActiveDirectory -ErrorAction Stop
 
+$outputFilePath = "{0}_AdminSDHolders.csv" -f $env:USERDOMAIN
+
 $domainDN = Get-ADDomain | Select-Object -Expand DistinguishedName
 
 # ACLs we want to target..any one of these needs to be considered high risk!
@@ -55,7 +57,7 @@ $identityAcls = $idMap.GetEnumerator() | ForEach-Object {
 
         $groupMembers = @()
         $groupMemberDNs | ForEach-Object {
-            $groupMembers += ($_.Split(",")[0].Replace("CN=", ""))
+            $groupMembers += ($_.Split(",")[0].Replace("CN=",""))
         }
 
         [IdentityAcl]::new($idFullName, $adRights, $true, $groupMembers)
@@ -68,12 +70,13 @@ $identityAcls = $idMap.GetEnumerator() | ForEach-Object {
 # Using Compare-Object and Where-Object, determine if any of the AD rights on the incoming objects exist in the $targetAdRightsToAudit array:
 $auditResults = $identityAcls | ForEach-Object {
     [int]$aclCount = Compare-Object -ReferenceObject $targetAdRightsToAudit -DifferenceObject $_.ActiveDirectoryRights -IncludeEqual |
-        Where-Object SideIndicator -eq "==" | Measure-Object | Select-Object -ExpandProperty Count
+    Where-Object SideIndicator -eq "==" | Measure-Object | Select-Object -ExpandProperty Count
 
     if ($aclCount -ge 1) {
         $_
     }
 }
 
+$auditResults | Export-Csv -Path $outputFilePath -NoTypeInformation
 $auditResults | Out-GridView -Title ("AdminSDHolders for $env:USERDNSDOMAIN")
 #---------------------------------------------------- END Find-AdminSDHolder.ps1 --------------------------------------------------------------------
