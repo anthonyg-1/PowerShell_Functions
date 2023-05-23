45cfdb57ce5d3949cf40c288be0773557fd23c57
Tony Guimelli <tony.guimelli@gmail.com>
2023-05-23 17:57:38 -0400

    Update Get-LogonContextForMappedDrive.ps1

--- Get-LogonContextForMappedDrive.ps1
+++ Get-LogonContextForMappedDrive.ps1
@@ -2,13 +2,14 @@ function Get-LogonContextForMappedDrive {
     [CmdletBinding()]
     param()
 
-    PROCESS {
+    Process {
         $mappedDrives = Get-WmiObject -Class Win32_MappedLogicalDisk
 
         $mappedDrives | ForEach-Object {
             $logonContext = "{0}\{1}" -f $env:USERDOMAIN, $env:USERNAME
             $logonSecurityContext = Get-WmiObject -Class Win32_LogonSession -Filter "Name='$($_.Name)'"
 
+            # If a login session context is detected, override default context value:
             if ($null -ne $logonSecurityContext) {
                 $logonContext = $logonSecurityContext
             }
