5fcf6f4e4c9d15b90bb4b473bccf5be52ece1054
Tony Guimelli <tony.guimelli@gmail.com>
2024-03-05 16:09:22 -0500

    Update Get-OutlookCalendar.ps1

--- Get-OutlookCalendar.ps1
+++ Get-OutlookCalendar.ps1
@@ -4,7 +4,7 @@ function Get-OutlookCalendar {
     [OutputType([PSCustomObject])]
     Param
     (
-        [Parameter(Mandatory = $true,
+        [Parameter(Mandatory = $false,
             ValueFromPipelineByPropertyName = $true, ParameterSetName = "Today",
             Position = 0)][Alias('t')][Switch]$Today,
 
@@ -17,6 +17,16 @@ function Get-OutlookCalendar {
             Position = 1)][Alias('end', 'e')][DateTime]$EndDate
     )
     BEGIN {
+        $targetPsVersion = 5
+        if ($PSVersionTable.PSVersion.Major -ne $targetPsVersion) {
+            $functionName = (Get-PSCallStack).Command[0]
+
+            $argExcepMessage = "{0} : This function will only run on PowerShell with a major version of {1}." -f $functionName, $targetPsVersion
+            $ArgumentException = New-Object -TypeName System.ArgumentException -ArgumentList $argExcepMessage
+            # Write-Error -Exception $ArgumentException -Category InvalidArgument -ErrorAction Stop
+            throw $ArgumentException
+        }
+
         Add-Type -Assembly "Microsoft.Office.Interop.Outlook" | Out-Null
 
         $olFolders = "Microsoft.Office.Interop.Outlook.OlDefaultFolders" -as [type]
