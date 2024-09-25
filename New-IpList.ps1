3de587370795ca6fb9b2e242bebf72f4ad9a38dc
Tony Guimelli <tony.guimelli@gmail.com>
2024-09-25 09:40:26 -0400

    Update New-IpList.ps1

--- New-IpList.ps1
+++ New-IpList.ps1
@@ -1,17 +1,40 @@
 function New-IpList {
     param (
-        [string]$BaseNetwork
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0,
+            ParameterSetName = 'IPV4Subnet')]
+        [ValidateLength(7, 15)]
+        [Alias('BaseNetwork', 's', 'is')]
+        $IPV4Subnet
     )
+    PROCESS {
+        [bool]$isIp = $false
+        try {
+            [IPAddress]::Parse($IPV4Subnet) | Out-Null
+            $isIp = $true
+        }
+        catch {
+            $isIp = $false
+        }
 
-    # Split base network into octets:
-    $octets = $BaseNetwork.Split('.')
+        if (-not($isIp)) {
+            $ArgumentException = [ArgumentException]::new("The following value passed to the IPV4Subnet parameter is not a valid IPv4 subnet: " -f $IPV4Subnet)
+            Write-Error -Exception $ArgumentException -Category ArgumentException -ErrorAction Stop
+        }
 
-    # First two octets from base network, iterate over 3rd and 4th octets (limit to 254):
-    for ($i = 0; $i -le 254; $i++) {
-        for ($j = 0; $j -le 254; $j++) {
-            $ip = "$($octets[0]).$($octets[1]).$i.$j"
-            # Write IP address to pipeline
-            Write-Output $ip
+        # Split base network into octets:
+        $octets = $IPV4Subnet.Split('.')
+
+        # First two octets from base network, iterate over 3rd and 4th octets (limit to 254):
+        for ($i = 0; $i -le 254; $i++) {
+            for ($j = 0; $j -le 254; $j++) {
+                $ip = "$($octets[0]).$($octets[1]).$i.$j"
+
+                # Write each IP address to pipeline:
+                Write-Output $ip
+            }
         }
     }
 }
