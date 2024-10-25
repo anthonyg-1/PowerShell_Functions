ae76923ff88f45475328f812ad1729e38449f9df
Tony Guimelli <tony.guimelli@gmail.com>
2024-10-25 11:44:53 -0400

    Update New-RandomPassword.ps1

--- New-RandomPassword.ps1
+++ New-RandomPassword.ps1
@@ -1,12 +1,35 @@
-function New-RandomPassword ([int]$Length = 24) {
-    $nonAlphaNumericCharacters = '!@#$%^&*'
-    $alphaNumericCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+function New-RandomPassword {
+    [CmdletBinding()]
+    [Alias('nrp')]
+    [OutputType([string], [void])]
+    param
+    (
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)][Alias('l')][int]$Length = 24,
 
-    $allowedCharacters += ($nonAlphaNumericCharacters, $alphaNumericCharacters)
+        [Parameter(Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)][Alias('c', 'clip')][Switch]$ToClipboard
 
-    # Starting in PowerShell 5, Get-Random uses RNGCryptoServiceProvider. See: https://www.sans.org/blog/truerng-random-numbers-with-powershell-and-math-net-numerics/
-    $randomPasswordString = ($allowedCharacters.ToCharArray() |
-        Sort-Object -Property { Get-Random })[1..$Length] -join ''
+    )
+    PROCESS {
+        $nonAlphaNumericCharacters = '!@#$%^&*'
+        $alphaNumericCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    return $randomPasswordString
+        $allowedCharacters += ($nonAlphaNumericCharacters, $alphaNumericCharacters)
+
+        # Starting in PowerShell 5, Get-Random uses RNGCryptoServiceProvider. See: https://www.sans.org/blog/truerng-random-numbers-with-powershell-and-math-net-numerics/
+        $randomPasswordString = ($allowedCharacters.ToCharArray() |
+            Sort-Object -Property { Get-Random })[1..$Length] -join ''
+
+        if ($PSBoundParameters.ContainsKey("ToClipboard")) {
+            $randomPasswordString | Set-Clipboard
+
+            Write-Warning -Message 'Newly generated password sent to the clipboard. Clear clipboard contents by running the following: scb $null'
+        }
+        else {
+            return $randomPasswordString
+        }
+    }
 }
